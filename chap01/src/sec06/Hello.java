4fd0771efc624ae0448588039e99ecacebcb811d
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-09-20 23:43:46 +0900

    .

--- chap01/src/sec06/Hello.java
+++ chap01/src/sec06/Hello.java
@@ -1,7 +1,19 @@
 package sec06;
+
 public class Hello {
 	public static void main(String[] args) {
-		String message = "안녕";
-		System.out.println(message);
+		int a = 1, b = 2;
+		switch (a++) {
+			case 1: b += a;
+			case 2: b += 5;
+		}
+		System.out.println(b);
+
+		int c = 1, d = 2;
+		switch (c++) {
+			case 1 -> d += c;
+			case 2 -> d += 5;
+		}
+		System.out.println(d);
 	}
 }
